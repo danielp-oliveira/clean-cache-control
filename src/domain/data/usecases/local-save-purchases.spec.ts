73da97481b712e3befbf4c4038b2c2e23f4d6388
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-07-15 14:42:57 -0300

    ensure LocalSavePurchases deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,18 +1,34 @@
 class LocalSavePurchases {
   constructor (private readonly cacheStore: CacheStore) { }
+
+  async save (): Promise<void> {
+    this.cacheStore.delete()
+  }
 }
 
 interface CacheStore {
+  delete: () => void
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0
+
+  delete (): void {
+    this.deleteCallsCount++
+  }
 }
 
 describe('LocalSavePurchases', () => {
   test('Should not delete cache on sut.init', () => {
     const cacheStoreSpy = new CacheStoreSpy()
-    const sut = new LocalSavePurchases(cacheStoreSpy)
+    new LocalSavePurchases(cacheStoreSpy)
     expect(cacheStoreSpy.deleteCallsCount).toBe(0)
+  })
+
+  test('Should delete old cache on sut.save', async () => {
+    const cacheStoreSpy = new CacheStoreSpy()
+    const sut = new LocalSavePurchases(cacheStoreSpy)
+    await sut.save()
+    expect(cacheStoreSpy.deleteCallsCount).toBe(1)
   })
 })
